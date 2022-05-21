82da2880b0db08f3c629ddffb8477925ade50f63
Ziryt <43845008+Ziryt@users.noreply.github.com>
2022-05-21 11:50:33 +0300

    Update lazy1p.sh

--- lazy1p.sh
+++ lazy1p.sh
@@ -18,3 +18,19 @@ mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@var /dev
 mount /dev/nvme1n1p1 /mnt/boot/efi
 
 lsblk
+
+pacstrap /mnt base linux linux-firmware git nano intel-ucode btrfs-progs
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+#chroot to /mnt
+arch-chroot /mnt
+#look fs table
+cat /etc/fstab
+git clone https://github.com/ziryt/arch-install
+cd arch-install/
+#edit base installer
+#nano base.sh
+chmod +x base.sh
+cd /
+./arch-install/base.sh
