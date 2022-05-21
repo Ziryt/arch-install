82e80e54befac5a082a2ffd8426044d80334ace3
Ziryt <43845008+Ziryt@users.noreply.github.com>
2022-05-21 12:08:07 +0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -12,12 +12,8 @@ git clone https://aur.archlinux.org/yay.git
 cd /yay
 makepkg -si --noconfirm
 
-sudo pacman -S --noconfirm xorg
-sudo pacman -S --noconfirm gdm gnome gnome-tweaks obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu 
-sudo pacman -S --noconfirm tf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts 
-sudo pacman -S --noconfirm ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code 
-sudo pacman -S --noconfirm ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans 
-sudo pacman -S --noconfirm gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+sudo pacman -S --noconfirm gdm gnome xorg gnome-tweaks obs-studio vlc 
+sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu tf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 yay -S --noconfirm --removemake telegram-desktop-bin discord google-chrome gnome-console qbittorrent menulibre
 
