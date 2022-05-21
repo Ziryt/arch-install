c677911158784b59da00b1ae2ba762d8e22dc9a7
Ziryt <43845008+Ziryt@users.noreply.github.com>
2022-05-21 11:46:08 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -4,20 +4,20 @@ sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=en_US.UTF-8" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
+echo "lmz" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1 lmz.localdomain lmz" >> /etc/hosts
 
 read -p 'Username: ' uservar
 read -sp 'Password: ' passvar
 
 echo root:$passvar | chpasswd
 
-pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
-#pacman -S cups hplip
-
-# pacman -S --noconfirm xf86-video-amdgpu
+pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers 
+pacman -S avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh 
+pacman -S rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld 
+pacman -S flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
@@ -25,7 +25,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
-#systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable reflector.timer
@@ -37,7 +36,6 @@ systemctl enable acpid
 useradd -m $uservar
 echo $uservar:$passvar | chpasswd
 usermod -aG libvirt $uservar
-
 
 echo "$uservar ALL=(ALL) ALL" >> /etc/sudoers.d/$uservar
 
