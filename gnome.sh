47354e849046ea10bb7701cd0d2f7292a9c6949a
Ziryt <43845008+Ziryt@users.noreply.github.com>
2022-06-09 20:56:49 +0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -12,11 +12,11 @@ git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si --noconfirm
 
-sudo pacman -S --noconfirm gdm gnome gnome-tweaks obs-studio vlc grub-customizer dconf-editor steam
+sudo pacman -S --noconfirm gdm gnome gnome-tweaks obs-studio vlc grub-customizer dconf-editor steam gimp
 sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu tf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 yay -S --noconfirm --removemake telegram-desktop-bin discord google-chrome gnome-console qbittorrent menulibre gimp visual-studio-code-bin
-yay -S --noconfirm --removemake bottles noisetorch
+yay -S --noconfirm --removemake bottles noisetorch bauh
 
 sudo systemctl enable gdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
